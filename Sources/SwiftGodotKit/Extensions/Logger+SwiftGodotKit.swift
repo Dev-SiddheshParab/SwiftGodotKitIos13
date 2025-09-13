c404beba6792f24661ee9ba6a4bca21cfb096444
Dev-SiddheshParab <siddheshparab.iosdev@gmail.com>
2025-09-13 13:17:57 +0530

    Update Logger+SwiftGodotKit.swift
    Updated Logger code

--- Sources/SwiftGodotKit/Extensions/Logger+SwiftGodotKit.swift
+++ Sources/SwiftGodotKit/Extensions/Logger+SwiftGodotKit.swift
@@ -2,7 +2,10 @@ import OSLog
 
 extension Logger {
     static let subsystem = "com.swiftgodotkit"
-    
+
+    @available(iOS 14.0, *)
     static let Window = Logger(subsystem: subsystem, category: "Window")
+
+    @available(iOS 14.0, *)
     static let App = Logger(subsystem: subsystem, category: "App")
 }
