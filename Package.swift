2f70e09bace2cd55fd158b7b6c56cd58372c9466
Mohammed Suhail Patel <suhailpatel94@Mohammeds-MacBook-Air.local>
2025-09-13 12:05:52 +0530

    add ios13 compile support

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SwiftGodotKit",
     platforms: [
         .macOS(.v14),
-        .iOS(.v17)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
