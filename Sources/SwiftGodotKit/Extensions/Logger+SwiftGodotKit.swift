99b3ad5e810accd0aab0fdeef6793014369b157e
Dev-SiddheshParab <siddheshparab.iosdev@gmail.com>
2025-09-13 13:30:19 +0530

    Update Logger+SwiftGodotKit.swift
    Fixing Logger issue for iOS 13

--- Sources/SwiftGodotKit/Extensions/Logger+SwiftGodotKit.swift
+++ Sources/SwiftGodotKit/Extensions/Logger+SwiftGodotKit.swift
@@ -1,11 +1,39 @@
 import OSLog
 
-extension Logger {
+import OSLog
+
+/// A unified logger that works on iOS 13 and 14+
+struct AppLogger {
+    private let subsystem: String
+    private let category: String
+
+    init(subsystem: String, category: String) {
+        self.subsystem = subsystem
+        self.category = category
+    }
+
+    func log(_ message: String) {
+        if #available(iOS 14.0, *) {
+            let logger = Logger(subsystem: subsystem, category: category)
+            logger.info("\(message, privacy: .public)")
+        } else {
+            os_log("%@", log: OSLog(subsystem: subsystem, category: category), type: .info, message)
+        }
+    }
+
+    func error(_ message: String) {
+        if #available(iOS 14.0, *) {
+            let logger = Logger(subsystem: subsystem, category: category)
+            logger.error("\(message, privacy: .public)")
+        } else {
+            os_log("%@", log: OSLog(subsystem: subsystem, category: category), type: .error, message)
+        }
+    }
+}
+
+extension AppLogger {
     static let subsystem = "com.swiftgodotkit"
 
-    @available(iOS 14.0, *)
-    static let Window = Logger(subsystem: subsystem, category: "Window")
-
-    @available(iOS 14.0, *)
-    static let App = Logger(subsystem: subsystem, category: "App")
+    static let Window = AppLogger(subsystem: subsystem, category: "Window")
+    static let App = AppLogger(subsystem: subsystem, category: "App")
 }
